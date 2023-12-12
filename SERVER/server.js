2ed6268d4eb9367ec45af750e793f62fce90343f
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-12-12 12:53:43 -0800

    HTML response

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,10 +1,13 @@
 // Building a server with node js
 
 const http = require('http');
+const fs = require('fs');
+
+const HTML = fs.readFileSync(`${__dirname}/index.html`)
 
 const server = http.createServer((req,res)=> {
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello, I am the response');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(HTML);
 });
 server.listen(8181,'127.0.0.1');
 console.log('server is running on localhost8181');
