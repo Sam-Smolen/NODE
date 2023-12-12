a9429f86b33aa626d70da50bb89a1131bab4bc48
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-12-12 13:17:36 -0800

    JSON response

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,13 +1,24 @@
 // Building a server with node js
 
 const http = require('http');
-const fs = require('fs');
+// const fs = require('fs');
 
-const HTML = fs.readFileSync(`${__dirname}/index.html`)
+// const HTML = fs.readFileSync(`${__dirname}/index.html`)
 
-const server = http.createServer((req,res)=> {
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(HTML);
-});
+const names = ['francis','james','rob'];
+const cars = {
+    name:"Ford",
+    model:"Fiesta"
+}
+
+const server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'application/json'});
+    const json = JSON.stringify({
+        names,
+        cars
+    });
+    res.end(json)
+})
+
 server.listen(8181,'127.0.0.1');
-console.log('server is running on localhost8181');
+console.log('Server is running on port 8181');
